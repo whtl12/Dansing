6ca83866e5249668bcdefa4ff20b3a1ece560bcf
Ahri <yjjo@maxst.com>
2017-02-20 13:41:36 +0900

    Test

---
+++ Assets/Controller.cs
@@ -0,0 +1,37 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Controller : MonoBehaviour {
+
+    public Transform[] Hand_2;
+    public RectTransform[] Hand_2_Node;
+
+    public Camera Main_Camera;
+    public Canvas MainCanvas;
+    RectTransform CanvasRect;
+
+    // Use this for initialization
+    void Start () {
+         CanvasRect = MainCanvas.GetComponent<RectTransform>();
+    }
+	
+	// Update is called once per frame
+	void Update () {
+
+        
+        for(int i =0; i < Hand_2.Length; i++)
+        {
+            Vector2 ViewportPosition = Main_Camera.WorldToViewportPoint(Hand_2[i].transform.position);
+            Vector2 WorldObject_ScreenPosition = new Vector2(
+            ((ViewportPosition.x * CanvasRect.sizeDelta.x) - (CanvasRect.sizeDelta.x * 0.5f)),
+            ((ViewportPosition.y * CanvasRect.sizeDelta.y) - (CanvasRect.sizeDelta.y * 0.5f)));
+
+            //now you can set the position of the ui element
+            Hand_2_Node[i].anchoredPosition = WorldObject_ScreenPosition;
+        }
+       
+      
+
+    }
+}
---
+++ Assets/GameManager.cs
@@ -0,0 +1,102 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class GameManager : MonoBehaviour {
+    public GameObject[] Ring;
+    public Animator[] RingAnim;
+
+    bool StartGame = false;
+    public Animator animator;
+
+    // Use this for initialization
+    void Start () {
+        animator.SetBool("Idling", true);
+    }
+	
+	// Update is called once per frame
+	void Update () {
+
+        if(StartGame == false)
+        {
+            int Rand = Random.Range(0, 4);
+
+            switch (Rand)
+            {
+                case 0:
+                case 1:
+                case 2:
+                case 3:
+                    StartCoroutine(OnRingObj(Rand));
+                    break;
+                default:
+                    break;
+            }
+
+        }
+
+    }
+
+    IEnumerator OnRingObj(int index)
+    {
+        StartGame = true;
+        float AnimSpeedRand = Random.Range(1.0f, 2.0f);
+        int RingNumber1 = Random.Range(0, 4);
+        int RingNumber2 = Random.Range(0, 4);
+
+        List<int> RingNumberList = new List<int>();
+
+       
+
+        if (index <= 1)
+        {
+            RingNumberList.Add(RingNumber1);
+        }
+        else
+        {
+            RingNumberList.Add(RingNumber1);
+            RingNumberList.Add(RingNumber2);
+        }
+
+        for(int i = 0; i < RingNumberList.Count; i++)
+        {
+            int RingIndex =  RingNumberList[i];
+
+            Ring[RingIndex].SetActive(true);
+            RingAnim[RingIndex].speed = AnimSpeedRand;
+            RingAnim[RingIndex].Play("RingAnim");
+        }
+
+        for (int i = 0; i < Ring.Length; i++)
+        {
+            if(RingNumberList.FindIndex(item => item == i) == -1)
+            {
+                Ring[i].SetActive(false);
+            }
+           
+        }
+
+        yield return new WaitForSeconds(1 / AnimSpeedRand);
+
+        for (int i = 0; i < Ring.Length; i++)
+        {
+            Ring[i].SetActive(false);
+            RingAnim[i].speed = 1;
+            RingAnim[i].Play("Idle");
+        }
+        StartGame = false;
+    }
+
+
+    public void ClickEventCheck(GameObject obj)
+    {
+        if(obj.activeSelf && obj.transform.localScale.x < 0.7f && obj.transform.localScale.x > 0.4f)
+        {
+            animator.SetTrigger("Pain");
+            Debug.Log("Click!");
+        }
+           
+
+    }
+
+}
